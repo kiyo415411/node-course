22a2282df30e69614b1c30dfb993f2d88a74ec4c
june <barbie841111@gmail.com>
2022-05-15 17:25:04 +0800

    crawier3.js

--- crawier/crawier3.js
+++ crawier/crawier3.js
@@ -4,22 +4,18 @@ const fs = require("fs/promises");
 (async () => {
   try {
     let stockNo = await fs.readFile("stock.txt", "utf-8");
-    console.log("read stockNo from file", stockNo);
-    try {
-      const response = await axios.get(
-        "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
-        {
-          params: {
-            response: "json",
-            date: "20220301",
-            stockNo: stockNo,
-          },
-        }
-      );
-      console.log(response.data);
-    } catch {
-      console.log("get data error");
-    }
+    // console.log("read stockNo from file", stockNo);
+    const response = await axios.get(
+      "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+      {
+        params: {
+          response: "json",
+          date: "20220301",
+          stockNo: stockNo,
+        },
+      }
+    );
+    console.log(response.data);
   } catch {
     console.log("read file error");
   }
