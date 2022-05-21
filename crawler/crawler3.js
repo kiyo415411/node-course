8701ef849ad5b58c089312de0fb1bcc2eec017d7
june <barbie841111@gmail.com>
2022-05-21 11:53:35 +0800

    test

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -1,27 +1,30 @@
+// npm i axios
 const axios = require("axios").default;
-const fs = require("fs/promises");
+// fs (filesystem) 內建的模組不用安裝，可以直接使用
+const fs = require("fs/promises"); // 使用 promise 版本
 
 (async () => {
-  try {
-    let stockNo = await fs.readFile("stock.txt", "utf-8");
-    console.log("read stockNo from file", stockNo);
-    // try {
-    const response = await axios.get(
-      "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
-      {
-        params: {
-          response: "json",
-          date: "20220301",
-          stockNo: stockNo,
-        },
-      }
-    );
-    console.log(response.data);
-    // } catch {
-    //   console.log("get data error");
-    // }
-  } catch (error) {
-    // console.log("read file error");
-    console.log(error);
-  }
+    try {
+        let stockNo = await fs.readFile("stock.txt", "utf-8");
+        console.log("read stockNo from file", stockNo);
+        // try {
+        const response = await axios.get(
+            "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+            {
+                params: {
+                    // 設定 query string
+                    response: "json",
+                    date: "20220301",
+                    stockNo: stockNo,
+                },
+            }
+        );
+        console.log(response.data);
+        // } catch {
+        //   console.log("get data error");
+        // }
+    } catch (error) {
+        // console.log("read file error");
+        console.log(error);
+    }
 })();
---
+++ crawler/crawler4.js
@@ -0,0 +1,20 @@
+// 從資料庫讀取股票代碼
+
+// mysql2 是一個第三方套件
+// npm i mysql2
+// 引用進來
+
+const mysql = require("mysql2/promise");
+(async () => {
+    const connection = await mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "admin",
+        password: "",
+        database: "stocks",
+    });
+    let [data, fields] = await connection.execute("SELECT * FROM stocks");
+    console.log(data);
+
+    connection.end();
+})();
