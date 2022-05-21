7b3bb74ea2b6eeba01e2fc27a82161769b109bbb
june <barbie841111@gmail.com>
2022-05-21 16:35:15 +0800

    add simple-express

--- simple-express/server.js
+++ simple-express/server.js
@@ -10,11 +10,23 @@ const app = express();
 // method: get, post, put, delete, ...
 
 app.get("/", (request, response, next) => {
-    response.send("首頁");
+    response.send("<html><body><h1 style='color:red;'>首頁</h1><body></html>");
 });
 
 app.get("/about", (request, response, next) => {
-    response.send("About Me");
+    response.send("關於我");
+});
+
+app.get("/news", (request, response, next) => {
+    response.send("最新消息");
+});
+
+app.get("/activity", (request, response, next) => {
+    response.send("活動");
+});
+
+app.get("/course", (request, response, next) => {
+    response.send("課程");
 });
 
 app.listen(3001, () => {
