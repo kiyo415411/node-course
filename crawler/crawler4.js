d98533e71f681baa9753bb462f5eb3f50fbafc65
june <barbie841111@gmail.com>
2022-05-21 14:15:39 +0800

    add .env.example

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -5,13 +5,18 @@
 // 引用進來
 
 const mysql = require("mysql2/promise");
+// const dotenv = require("dotenv");
+// dotenv.config();
+require("dotenv").config();
+
 (async () => {
+    // console.log("DB_HOST", process.env.DB_HOST);
     const connection = await mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "admin",
-        password: "",
-        database: "stocks",
+        host: process.env.DB_HOST,
+        port: process.env.DB_POST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     });
     let [data, fields] = await connection.execute("SELECT * FROM stocks");
     console.log(data);
