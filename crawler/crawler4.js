83a4b9702f2954cf299220e10892c1681159aa4b
june <barbie841111@gmail.com>
2022-05-21 11:27:25 +0800

    add crawler4

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -1,23 +1,20 @@
-const mysql = require("mysql2/promise");
+// 從資料庫讀取股票代碼
 
+// mysql2 是一個第三方套件
+// npm i mysql2
+// 引用進來
+
+const mysql = require("mysql2/promise");
 (async () => {
     const connection = await mysql.createConnection({
         host: "localhost",
-        post: "3306",
+        port: 3306,
         user: "admin",
         password: "123456",
         database: "stocks",
     });
-
     let [data, fields] = await connection.execute("SELECT * FROM stocks");
     console.log(data);
 
-    // results [
-    //     [],
-    //     []
-    // ]
-
-    // let data = results[0]
-    // let fields = results[1]
     connection.end();
 })();
